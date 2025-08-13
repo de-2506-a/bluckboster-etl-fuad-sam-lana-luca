30d6c9a3a1aeb9e69ad3d0600236690d1e8ed1e9
svet-g <svetlana.gljadelkina@gmail.com>
2025-08-13 15:48:08 +0100

    fix: more mess

--- streamlit/nav_pages/customers.py
+++ streamlit/nav_pages/customers.py
@@ -11,26 +11,26 @@ df = get_customer_by_city_and_country()
 def show_customers():
     st.header("Customer Insights")
     
-# Summary statistics
-st.subheader("Summary Statistics")
-col1, col2, col3 = st.columns(3)
+    # Summary statistics
+    st.subheader("Summary Statistics")
+    col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.metric("Total Cities", len(df))
+    with col1:
+        st.metric("Total Cities", len(df))
 
-with col2:
-    st.metric("Total Countries", len(df['country'].unique()))
+    with col2:
+        st.metric("Total Countries", len(df['country'].unique()))
 
-with col3:
-    st.metric("Total Customers", df['total_customers'].sum())
+    with col3:
+        st.metric("Total Customers", df['total_customers'].sum())
 
-# Country analysis
-customers_by_country = df.groupby('country', as_index=False)['total_customers'].sum().sort_values(ascending=False, by='total_customers')
-fig_country = px.bar(customers_by_country, x='total_customers', y='country',
-                     title="Total Customers by Country",
-                     color='total_customers')
-fig_country.update_layout(
-    height=1000,
-    width=700
-)
-st.plotly_chart(fig_country)
+    # Country analysis
+    customers_by_country = df.groupby('country', as_index=False)['total_customers'].sum().sort_values(ascending=False, by='total_customers')
+    fig_country = px.bar(customers_by_country, x='total_customers', y='country',
+                        title="Total Customers by Country",
+                        color='total_customers')
+    fig_country.update_layout(
+        height=1000,
+        width=700
+    )
+    st.plotly_chart(fig_country)
